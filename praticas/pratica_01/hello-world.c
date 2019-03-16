9c8ba21de6bb0e49b4c3cf9ceddb9b6421b468db
Matheus Frata <matheus.frata@gmail.com>
2019-03-16 19:44:46 -0300

    Add hello world

---
+++ praticas/pratica_01/hello-world.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "contiki.h"
+
+
+PROCESS(hello_world_process, "Hello world process");
+AUTOSTART_PROCESSES(&hello_world_process);
+
+PROCESS_THREAD(hello_world_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  /* Insira o codigo aqui */
+
+  
+  /************************/
+
+  PROCESS_END();
+}
