e61a4dbeacd92e44eab10702fbaca75601361270
Arliones Hoeller Jr <arliones@gmail.com>
2019-04-02 04:12:56 -0300

    Adiciona Pratica 02

--- praticas/pratica_02/sensor-timer-uart.c
+++ praticas/pratica_02/sensor-timer-uart.c
@@ -7,14 +7,15 @@
 
 /*---------------------------------------------------------------------------*/
 #define BUF_SIZE 8
-static struct etimer et_sensor;
-static struct etimer et_uart;
 static int buffer[BUF_SIZE];
 static int buf_c = 0;
 
+static struct etimer et_sensor;
+static struct etimer et_uart;
+
 /*---------------------------------------------------------------------------*/
 PROCESS(sensor_process, "Sensor process");
-PROCESS(uart_process, "UART process");
+PROCESS(uart_process, "Serial process");
 
 /*---------------------------------------------------------------------------*/
 AUTOSTART_PROCESSES(&sensor_process,&uart_process);
@@ -25,7 +26,7 @@ PROCESS_THREAD(sensor_process, ev, data)
 
   SENSORS_ACTIVATE(batmon_sensor);
 
-  etimer_set(&et_sensor, CLOCK_SECOND); // a cada segundo
+  etimer_set(&et_sensor, 1*CLOCK_SECOND); // a cada segundo
 
   while(1) {
     PROCESS_WAIT_EVENT();
@@ -35,8 +36,9 @@ PROCESS_THREAD(sensor_process, ev, data)
 
         int val = batmon_sensor.value(BATMON_SENSOR_TYPE_TEMP); // lê sensor
 
-        buffer[buf_c++] = val; // atualiza buffer
-        buf_c = buf_c % BUF_SIZE;
+        /* Insira seu código aqui */
+
+
 
         printf("Leu %d\n", val);
     }
@@ -50,13 +52,13 @@ PROCESS_THREAD(uart_process, ev, data)
 {
   PROCESS_BEGIN();
 
-  etimer_set(&et_uart, CLOCK_SECOND*10); // a cada 10 segundos
+  /* Insira seu código aqui */
 
   while(1) {
     PROCESS_WAIT_EVENT();
     if(ev == PROCESS_EVENT_TIMER)  // se passaram 10 segundos
     {
-        etimer_reset(&et_uart); // reinicia timer
+        /* Insira seu código aqui */
 
         int avg = 0;
         for(int i=0;i<BUF_SIZE;i++) {
