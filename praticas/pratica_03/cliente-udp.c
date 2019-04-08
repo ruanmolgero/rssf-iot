4fd460e4e8c55a3d78ca3a3665b793f4a0ddf00a
Arliones Hoeller Jr <arliones@gmail.com>
2019-04-08 16:54:30 -0300

    Add "Atividade 1".

--- praticas/pratica_03/cliente-udp.c
+++ praticas/pratica_03/cliente-udp.c
@@ -1,227 +1,88 @@
-/*
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
-
 #include "contiki.h"
-#include "contiki-lib.h"
 #include "contiki-net.h"
 #include "net/ip/resolv.h"
-#include "dev/leds.h"
 
-#include <string.h>
-#include <stdbool.h>
+#include <stdio.h>
 
-#define DEBUG DEBUG_PRINT
-#include "net/ip/uip-debug.h"
+#include "utils.h"
 
-#define SEND_INTERVAL		(15 * CLOCK_SECOND)
-#define MAX_PAYLOAD_LEN		(40)
-#define CONN_PORT     (8802)
-#define MDNS (1)
 
-static char buf[MAX_PAYLOAD_LEN];
 
+
+
+static struct etimer et;
 static struct uip_udp_conn *client_conn;
+static uip_ipaddr_t ipaddr;
 
-#define UIP_UDP_BUF  ((struct uip_udp_hdr *)&uip_buf[UIP_LLH_LEN + UIP_IPH_LEN])
-#define UIP_IP_BUF   ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
+
+
 
 /*---------------------------------------------------------------------------*/
-PROCESS(udp_client_process, "UDP client process");
-AUTOSTART_PROCESSES(&resolv_process,&udp_client_process);
+PROCESS(udp_client_process, "Processo UDP cliente");
+
 /*---------------------------------------------------------------------------*/
-static void
-tcpip_handler(void)
-{
-    char *dados;
-
-    if(uip_newdata()) {
-        dados = uip_appdata;
-        dados[uip_datalen()] = '\0';
-        printf("Response from the server: '%s'\n", dados);
-    }
-}
-/*---------------------------------------------------------------------------*/
-static void
-timeout_handler(void)
-{
-    char payload = 0;
-
-    buf[0] = payload;
-    if(uip_ds6_get_global(ADDR_PREFERRED) == NULL) {
-      PRINTF("Aguardando auto-configuracao de IP\n");
-      return;
-    }
-    uip_udp_packet_send(client_conn, buf, strlen(buf));
-}
-/*---------------------------------------------------------------------------*/
-static void
-print_local_addresses(void)
-{
-  int i;
-  uint8_t state;
-
-  PRINTF("Client IPv6 addresses: ");
-  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
-    state = uip_ds6_if.addr_list[i].state;
-    if(uip_ds6_if.addr_list[i].isused &&
-       (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
-      PRINT6ADDR(&uip_ds6_if.addr_list[i].ipaddr);
-      PRINTF("\n");
-    }
-  }
-}
-/*---------------------------------------------------------------------------*/
-#if UIP_CONF_ROUTER
-static void
-set_global_address(void)
-{
-  uip_ipaddr_t ipaddr;
-
-  uip_ip6addr(&ipaddr, UIP_DS6_DEFAULT_PREFIX, 0, 0, 0, 0, 0, 0, 0);
-  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
-  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
-}
-#endif /* UIP_CONF_ROUTER */
-/*---------------------------------------------------------------------------*/
-
-#if MDNS
-
-static resolv_status_t
-set_connection_address(uip_ipaddr_t *ipaddr)
-{
-#ifndef UDP_CONNECTION_ADDR
-#if RESOLV_CONF_SUPPORTS_MDNS
-#define UDP_CONNECTION_ADDR       contiki-udp-server.local
-#elif UIP_CONF_ROUTER
-#define UDP_CONNECTION_ADDR       fd00:0:0:0:0212:7404:0004:0404
-#else
-#define UDP_CONNECTION_ADDR       fe80:0:0:0:6466:6666:6666:6666
-#endif
-#endif /* !UDP_CONNECTION_ADDR */
-
-#define _QUOTEME(x) #x
-#define QUOTEME(x) _QUOTEME(x)
-
-    resolv_status_t status = RESOLV_STATUS_ERROR;
-
-    if(uiplib_ipaddrconv(QUOTEME(UDP_CONNECTION_ADDR), ipaddr) == 0) {
-        uip_ipaddr_t *resolved_addr = NULL;
-        status = resolv_lookup(QUOTEME(UDP_CONNECTION_ADDR),&resolved_addr);
-        if(status == RESOLV_STATUS_UNCACHED || status == RESOLV_STATUS_EXPIRED) {
-            PRINTF("Attempting to look up %s\n",QUOTEME(UDP_CONNECTION_ADDR));
-            resolv_query(QUOTEME(UDP_CONNECTION_ADDR));
-            status = RESOLV_STATUS_RESOLVING;
-        } else if(status == RESOLV_STATUS_CACHED && resolved_addr != NULL) {
-            PRINTF("Lookup of \"%s\" succeded!\n",QUOTEME(UDP_CONNECTION_ADDR));
-        } else if(status == RESOLV_STATUS_RESOLVING) {
-            PRINTF("Still looking up \"%s\"...\n",QUOTEME(UDP_CONNECTION_ADDR));
-        } else {
-            PRINTF("Lookup of \"%s\" failed. status = %d\n",QUOTEME(UDP_CONNECTION_ADDR),status);
-        }
-        if(resolved_addr)
-            uip_ipaddr_copy(ipaddr, resolved_addr);
-    } else {
-        status = RESOLV_STATUS_CACHED;
-    }
-
-    return status;
-}
-#endif
+AUTOSTART_PROCESSES(&resolv_process, &udp_client_process);
 
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(udp_client_process, ev, data)
 {
-  static struct etimer et;
-  uip_ipaddr_t ipaddr;
-
   PROCESS_BEGIN();
-  PRINTF("UDP client process -- started\n");
 
-#if 0 //UIP_CONF_ROUTER
-  set_global_address();
-#else
+  printf("Processo UDP cliente -- iniciou\r\n");
+
+  // Bloqueia até que configuração IPv6 seja bem sucedida (SLAAC)
   etimer_set(&et, 2*CLOCK_SECOND);
   while(uip_ds6_get_global(ADDR_PREFERRED) == NULL)
   {
       PROCESS_WAIT_EVENT();
       if(etimer_expired(&et))
       {
-          PRINTF("Aguardando auto-configuracao de IP\n");
           etimer_set(&et, 2*CLOCK_SECOND);
+          printf("Aguardando auto-configuracao de IPv6\r\n");
       }
   }
-#endif
-
+  // Imprime IPv6.
   print_local_addresses();
 
-#if MDNS
+
+  // Configura nome local (mDNS).
   char contiki_hostname[16];
-  sprintf(contiki_hostname,"node%02X%02X",linkaddr_node_addr.u8[6], linkaddr_node_addr.u8[7]);
+  sprintf(contiki_hostname,"sensor %02X%02X",linkaddr_node_addr.u8[6], linkaddr_node_addr.u8[7]);
   resolv_set_hostname(contiki_hostname);
-  PRINTF("Setting hostname to %s\n",contiki_hostname);
+  printf("Configurando hostname para %s\r\n",contiki_hostname);
 
+
+  /********** CONECTANDO AO SERVIDOR UDP **********/
+  // Encontrar endereço IPv6 do servidor
   static resolv_status_t status = RESOLV_STATUS_UNCACHED;
-  while(status != RESOLV_STATUS_CACHED) {
-      status = set_connection_address(&ipaddr);
+  while(status != RESOLV_STATUS_CACHED)
+  {
+      status = set_connection_address(&ipaddr, UDP_CONNECTION_ADDR);
 
-      if(status == RESOLV_STATUS_RESOLVING) {
-          //PROCESS_WAIT_EVENT_UNTIL(ev == resolv_event_found);
+      if(status == RESOLV_STATUS_RESOLVING)
+      {
           PROCESS_WAIT_EVENT();
-      } else if(status != RESOLV_STATUS_CACHED) {
-          PRINTF("Can't get connection address.\n");
-          //PROCESS_YIELD();
+      }
+      else if(status != RESOLV_STATUS_CACHED)
+      {
+          printf("Não foi possível obter IPv6 do servidor.\r\n");
           PROCESS_WAIT_EVENT();
       }
   }
-#else
-  //configures the destination IPv6 address
-  uip_ip6addr(&ipaddr, 0xfe80, 0, 0, 0, 0x215, 0x2000, 0x0002, 0x2145);
-#endif
-  /* new connection with remote host */
+  // Se não utilizar mDNS, use a linha abaixo para registrar IPv6 destino
+  // uip_ip6addr(&ipaddr, 0xfe80, 0, 0, 0, 0x215, 0x2000, 0x0002, 0x2145);
+
+  // Criando socket UDP para conexão com host:porta remoto
   client_conn = udp_new(&ipaddr, UIP_HTONS(CONN_PORT), NULL);
   udp_bind(client_conn, UIP_HTONS(CONN_PORT));
 
-  PRINT6ADDR(&client_conn->ripaddr);
-  PRINTF(" local/remote port %u/%u\n",
-	UIP_HTONS(client_conn->lport), UIP_HTONS(client_conn->rport));
+  printf("Servidor UDP: ");
+  printipv6(&client_conn->ripaddr);
+  printf("\r\nPorta local: %u.\r\nPorta remota: %u.\r\n", UIP_HTONS(client_conn->lport),
+                                                    UIP_HTONS(client_conn->rport));
 
-  etimer_set(&et, SEND_INTERVAL);
-  while(1) {
-    PROCESS_YIELD();
-    if(etimer_expired(&et)) {
-      timeout_handler();
-      etimer_restart(&et);
-    } else if(ev == tcpip_event) {
-      tcpip_handler();
-    }
-  }
+  // Se chegou até aqui, então a conexão foi bem sucedida!
+  // Toca para o próximo experimento! =)
 
   PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
--- praticas/pratica_03/project-conf.h
+++ praticas/pratica_03/project-conf.h
@@ -1,42 +1,16 @@
-/*
- * Copyright (c) 2015, SICS Swedish ICT.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- */
+/* Definições gerais para a prática 03 */
 
-/**
- * \author Simon Duquennoy <simonduq@sics.se>
- */
+// Qual canal vamos usar?
+#define RF_CORE_CONF_CHANNEL (0)
 
-#ifndef __PROJECT_CONF_H__
-#define __PROJECT_CONF_H__
+// Como está estruturada a informação a ser transmitida?
+typedef union {
+  int8_t i8[4];
+  int32_t i32;
+} int_payload_t;
 
-#undef ENERGEST_CONF_ON
-#define ENERGEST_CONF_ON (0)
-#define CC26XX_WEB_DEMO_6LBR_CLIENT (1)
+// Qual o endereço do servidor UDP?
+#define UDP_CONNECTION_ADDR       "contiki-udp-server.local"   // se usar mDNS
 
-#endif /* __PROJECT_CONF_H__ */
+// A qual porta vamos nos conectar?
+#define CONN_PORT 8802
